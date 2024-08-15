edcb1dae2905ed31c7c01017dc29556e25deab3c
Pinhuan Wang <pw346@raritan.engr>
2024-08-15 11:06:33 -0400

    Add main.cpp

--- build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -1,12 +1,14 @@
 file(REMOVE_RECURSE
   "CMakeFiles/MyExecutable.dir/cmake_device_link.o"
-  "CMakeFiles/MyExecutable.dir/src/deepwalk.cu.o"
-  "CMakeFiles/MyExecutable.dir/src/deepwalk.cu.o.d"
+  "CMakeFiles/MyExecutable.dir/src/apps/deepwalk.cu.o"
+  "CMakeFiles/MyExecutable.dir/src/apps/deepwalk.cu.o.d"
+  "CMakeFiles/MyExecutable.dir/src/main.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/main.cpp.o.d"
   "MyExecutable"
   "MyExecutable.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CUDA)
+foreach(lang CUDA CXX)
   include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
---
+++ build/CMakeFiles/MyExecutable.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyExecutable.dir/src/main.cpp.o: \
+ /home/pw346/Bingo/src/main.cpp /usr/include/stdc-predef.h \
+ /home/pw346/Bingo/include/apps.h
